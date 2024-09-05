55cb5fb26aa747aa566e38f854ed8d805396cd8f
Paula <psanzilundain@gmail.com>
2024-09-05 11:07:50 +0200

    counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,31 @@
 import '../scss/App.scss';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const sum = (a) => {
+    return a + 1;
+  };
+  const rest = (a) => {
+    return a - 1;
+  };
+  const handleSum = () => {
+    console.log('click');
+    setCounter(sum(counter));
+  };
+  const handleRest = () => {
+    setCounter(rest(counter));
+  };
+  const handleReset = () => {
+    setCounter(0);
+  };
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <h1>Contador: {counter}</h1>
+      <button onClick={handleSum}>+</button>
+      <button onClick={handleRest}>-</button>
+      <button onClick={handleReset}>reset</button>
     </div>
   );
 }
