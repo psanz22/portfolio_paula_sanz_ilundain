9b65e7235a780ee89de580039c9ab0bed40f0f02
Paula <psanzilundain@gmail.com>
2024-09-05 10:37:09 +0200

    ev

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
 import '../scss/App.scss';
 
 function App() {
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
+  const handleKeyUp = (ev) => {
+    console.log('keyup', ev.target.value);
+  };
+
   return (
-    <div>
-      <h1>Hola mundo</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type='text' onKeyUp={handleKeyUp} />
+    </form>
   );
 }
 
