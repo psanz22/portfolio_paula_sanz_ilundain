9785562bacadd9dd50c55ac82755a26800da026d
Paula <psanzilundain@gmail.com>
2024-09-19 21:07:55 +0200

    header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,48 @@
 import '../scss/App.scss';
-import Header from '../components/Header';
-import Main from '../components/Main';
-import Footer from '../components/Footer';
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <header className='header'>
+        <div>
+          <img
+            src='https://png.pngtree.com/png-vector/20190630/ourmid/pngtree-lovely-moon-icon-for-your-project-png-image_1532228.jpg'
+            alt='main page'
+            className='header__img'
+          />
+        </div>
+        <ul className='header__nav'>
+          <li>
+            <a href='#'>ABOUT ME</a>
+          </li>
+          <li>
+            <a href='#'>PROYECTOS</a>
+          </li>
+          <li>
+            <a href='#'>CONTACTOS</a>
+          </li>
+        </ul>
+      </header>
+      {/* <main>
+        <aside>
+          <img
+            src='https://static.vecteezy.com/system/resources/previews/012/002/361/original/me-letter-logo-design-initial-letters-me-logo-icon-abstract-letter-me-minimal-logo-design-template-m-e-letter-design-with-black-colors-me-logo-vector.jpg'
+            alt='about me'
+          />
+        </aside>
+        <section>
+          <img
+            src='https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2021/12/arcane-jinx-2575357.jpg?tf=3840x'
+            alt='proyectos'
+          />
+        </section>
+      </main>
+      <footer>
+        <ul>
+          <a href=''>Linkedin</a>
+          <a href=''>Github</a>
+        </ul>
+      </footer> */}
     </>
   );
 }
