d66eb045e2bd5028b33f54178bda7e26308c5be1
Paula <psanzilundain@gmail.com>
2024-09-19 21:44:39 +0200

    component footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,14 @@
 import '../scss/App.scss';
 import Header from '../components/Header';
 import Main from '../components/Main';
+import Footer from '../components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
-      <footer className='footer'>
-        <ul className='footer__nav'>
-          <a href='' className='footer__nav__linkedin'>
-            Linkedin
-          </a>
-          <a href='' className='footer__nav__git'>
-            Github
-          </a>
-        </ul>
-      </footer>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import '../scss/Footer.scss';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <ul className='footer__nav'>
+        <a href='' className='footer__nav__linkedin'>
+          Linkedin
+        </a>
+        <a href='' className='footer__nav__git'>
+          Github
+        </a>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
