0706001004a000ed5e16d2386302f8d0c1a12397
Paula <psanzilundain@gmail.com>
2024-09-19 21:13:26 +0200

    component header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,10 @@
 import '../scss/App.scss';
+import Header from '../components/Header';
 
 function App() {
   return (
     <>
-      <header className='header'>
-        <div>
-          <img
-            src='https://png.pngtree.com/png-vector/20190630/ourmid/pngtree-lovely-moon-icon-for-your-project-png-image_1532228.jpg'
-            alt='main page'
-            className='header__img'
-          />
-        </div>
-        <ul className='header__nav'>
-          <li>
-            <a href='#'>ABOUT ME</a>
-          </li>
-          <li>
-            <a href='#'>PROYECTOS</a>
-          </li>
-          <li>
-            <a href='#'>CONTACTOS</a>
-          </li>
-        </ul>
-      </header>
+      <Header />
       {/* <main>
         <aside>
           <img
