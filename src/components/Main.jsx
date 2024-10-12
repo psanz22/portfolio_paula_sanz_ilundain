0b133350ac874520771347fe815e600e2bfaa3b2
Paula <psanzilundain@gmail.com>
2024-10-12 14:36:31 +0200

    carroussel text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,16 +4,21 @@ function Main() {
   return (
     <main className='main'>
       <aside className='main__aside'>
-        <img
+        {/* <img
           src='https://static.vecteezy.com/system/resources/previews/012/002/361/original/me-letter-logo-design-initial-letters-me-logo-icon-abstract-letter-me-minimal-logo-design-template-m-e-letter-design-with-black-colors-me-logo-vector.jpg'
           alt='about me'
           className='main__aside__img'
-        />
+        /> */}
+        <button className='main__aside-button'>me</button>
         <div className='main__aside__containerText'>
-          <h1 className='main__aside__title'>Hello world!</h1>
+          <h1 className='main__aside__title'>Junior Full Stack Developer </h1>
           <p className='main__aside__title-2'>
             Soy Paula: desarrolladora junior fullstack.
           </p>
+        </div>
+        <div className='main__aside__rrss'>
+          <button className='main__aside__rrss-button'>Linkedin</button>
+          <button className='main__aside__rrss-button'>GitHub</button>
         </div>
       </aside>
       <section className='main__section'>
@@ -25,6 +30,16 @@ function Main() {
           />
         </div>
       </section>
+      <aside className='main__aside__moving slider '>
+        <div className='slide-track'>
+          <div className='slide'>
+            <h1 className='slide__title'>PROYECTOS DESTACADOS</h1>
+          </div>
+          <div className='slide'>
+            <h1 className='slide__title'>PROYECTOS DESTACADOS</h1>
+          </div>
+        </div>
+      </aside>
     </main>
   );
 }
