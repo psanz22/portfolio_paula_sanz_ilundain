25e9924fa02f4c83587313d33bba14e35aebaea5
Paula <psanzilundain@gmail.com>
2025-02-03 18:38:46 +0100

    formulario contacto

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,20 +3,29 @@ import '../scss/AboutMe.scss';
 function AboutMe() {
   return (
     <>
-      <h1>
-        Hola, soy desarrolladora Full Stack Junior certificada en AWS Cloud.
-      </h1>
-      <p>
-        Me encargo de todo el proceso de desarrollo de aplicaciones web gracias
-        a mi perfil full-stack, tengo un perfil versatil gracias a mis
-        trayectoria profesional previa, hablo tres idiomas con fluidez y
-        recientemente aprobé el certificado AWS Cloud Practitioner.
-      </p>
-      <h2>SKILLS</h2>
-      <p>
-        AWS · HTML5· CSS3 · SASS· JavaScript · React · Node · Express · SQL ·
-        Python · Git · Responsive Design
-      </p>
+      <div className='aboutMe'>
+        <h1 className='aboutMe__h1'>
+          Hola,<br></br> Soy desarrolladora Full Stack Junior certificada en AWS
+          Cloud.
+        </h1>
+        <p className='aboutMe__p'>
+          Me encargo de todo el proceso de desarrollo de aplicaciones web
+          gracias a mi perfil full-stack, tengo un perfil versatil gracias a mis
+          trayectoria profesional previa, hablo tres idiomas con fluidez y
+          recientemente aprobé el certificado AWS Cloud Practitioner.
+        </p>
+      </div>
+      <div>
+        <button>CONTACTO</button>
+        <button>CV</button>
+      </div>
+      <div className='skills'>
+        <h2 className='skills__h2'>SKILLS</h2>
+        <p className='skills__p'>
+          AWS · HTML5· CSS3 · SASS· JavaScript · React · Node · Express · SQL ·
+          Python · Git · Responsive Design
+        </p>
+      </div>
     </>
   );
 }
--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import '../scss/Contacto.scss';
 
 function Contacto() {
-  return <div>Contacto</div>;
+  return (
+    <>
+      <form className='form'>
+        <label id='name'></label>
+        <input
+          type='text'
+          id='name'
+          name='name'
+          placeholder='Tu nombre'
+          required
+        />
+        <label id='email'></label>
+        <input
+          type='text'
+          id='email'
+          name='email'
+          placeholder='Correo electrónico'
+          required
+        />
+        <label id='topic'></label>
+        <input
+          type='text'
+          id='topic'
+          name='topic'
+          placeholder='Asunto'
+          required
+        />
+        <label id='message'></label>
+        <input
+          type='text'
+          id='message'
+          name='message'
+          placeholder='Escribe aquí...'
+        />
+      </form>
+    </>
+  );
 }
 
 export default Contacto;
