f7ce46a0798a64f28492e20116b62bd8c9d03bfe
Paula <psanzilundain@gmail.com>
2025-02-06 19:11:03 +0100

    fixing main section

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,14 +3,16 @@ import terror from '/randomTerror.png';
 function MainSection() {
   return (
     <section className='main__section'>
-      <div className='main__section__carroussel'>
-        <h2 className='main__section__title'>Random Terror</h2>
+      <div className='main__section__carousel'>
         <img src={terror} alt='proyectos' className='main__section__img' />
-        <p>
-          Proyecto Full Stack de interfaz sencilla. Cuando el usuario da click,
-          la aplicación sugiere una película de terror. Tecnologías: HTML, CSS,
-          React, SQL, Node.js
-        </p>
+        <div className='main__section__carousel__wrapperText'>
+          <h3 className='main__section__carousel__title'>Random Terror</h3>
+          <p className='main__section__carousel__description'>
+            Proyecto Full Stack de interfaz sencilla. Cuando el usuario da
+            click, la aplicación sugiere una película de terror. Tecnologías:
+            HTML, CSS, React, SQL, Node.js
+          </p>
+        </div>
       </div>
     </section>
   );
